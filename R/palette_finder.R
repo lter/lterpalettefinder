b2bbda699b3823d8d949a4bbd7ea8fcc317a6905
njlyon0 <nicholasjlyon@gmail.com>
2022-04-27 11:13:41 -0700

    Made formatting accurate for roxygen

--- R/palette_finder.R
+++ R/palette_finder.R
@@ -1,115 +1,61 @@
-## ------------------------------------------------------------- ##
-         # LTER Network Office - Palette Finder Function
-## ------------------------------------------------------------- ##
-# Written by: Nick J Lyon
-
-# Purpose
-## Create function for easy identification of palettes from LTER photos that meet a user's needs (i.e., palette length, site origin, palette type, etc.)
-
-# Clear environment
-rm(list = ls())
-
-# Call needed libraries
-library(dplyr); library(tidyr); library(magrittr)
-
-# Grab working directory (defaults to project directory which we want)
-wd <- getwd()
-wd
-
-# Palette Finder Function ---------------------------------------------------
-
-# Acquire tidy data
-palette_options <- read.csv(file.path("Data", "palette_table.csv"))
-head(palette_options)
-
-# Function challenges -----------------
-
-# - Figure out how to embed palette dataframe into the function (maybe something that is done at package level and not locally?)
-
-# - Not a function issue per se, but need (1) palettes from more sites (2) longer (>10 colors) and (3) more palette types (sequential, diverging, qualitative)
-
-# Current variant of the function -------
-lter_palette_finder <- function(site = "all", name = "all", type = "all", length = "all"){
-  
-  # If any argument is unspecified it defaults to "all" and shouldn't be used as a subset condition.
-  # Change the input to be a vector of all options in the data in these cases.
+#' @title Find an LTER Palette
+#' 
+#' @description From a dataframe of all possible palettes (updated periodically so check back!) specify the characteristics of the palette you want and retrieve the palettes that match those criteria. Can specify by number of colors in the palette, type of palette (e.g., qualitative, sequential, etc.), or which LTER site the palette came from.
+#' 
+#' @param site Vector of three-letter LTER site abbreviations for which to return palettes
+#' @param name Vector of palette names (if known) for which to return palettes
+#' @param type Vector of palette types (e.g., qualitative, sequential, etc.) for which to return palettes
+#' @param length Vector of acceptable palette lengths (i.e., how many colors are needed)
+#' 
+#' @return If more than one palette is a dataframe, if exactly one palette is a vector 
+#' @export
+#'
+#' @examples
+#' # Look at all palette options by calling the function without specifying arguments
+#' lterpalettefinder::palette_finder()
+#'
+#' # What if our query returns NO options?
+#' lter_palette_finder(length = 1)
+#' 
+#' # What if our query returns MULTIPLE options?
+#' lter_palette_finder(length = 5, site = "HBR")
+#' 
+#' # What if our query returns JUST ONE option? (this is desirable)
+#' lter_palette_finder(site = "AND")
+#' 
+palette_finder <- function(site = "all", name = "all", type = "all", length = "all"){
+  # Handle unspecified arguments
   if(site == "all"){ site <- unique(palette_options$lter_site) }
   if(name == "all"){ name <- unique(palette_options$palette_name) }
   if(type == "all"){ type <- unique(palette_options$palette_type) }
   if(length == "all"){ length <- unique(palette_options$palette_length) }
   
-  # Subset to selected sites
+  # Subset by each condition
   palt_v1 <- dplyr::filter(palette_options, lter_site %in% site)
-  
-  # Subset to selected names
   palt_v2 <- dplyr::filter(palt_v1, palette_name %in% name)
-  
-  # Subset to selected types
   palt_v3 <- dplyr::filter(palt_v2, palette_type %in% type)
-  
-  # Subset to selected lengths
   palt_v4 <- dplyr::filter(palt_v3, palette_length %in% length)
   
-  # Pivot to long format
-  palt_v5 <- tidyr::pivot_longer(data = palt_v4,
-                                 cols = dplyr::starts_with('color'),
-                                 names_to = 'color_num',
-                                 values_to = 'color_hex')
-  
-  # Keep only hexes that are correct (need to be 7 characters long)
+  # Remove color columns that have no entries in the given subset
+  palt_v5 <- tidyr::pivot_longer(data = palt_v4, cols = dplyr::starts_with('color'),
+                                 names_to = 'color_num', values_to = 'color_hex')
   palt_v6 <- dplyr::filter(palt_v5, nchar(color_hex) == 7)
-  
-  # Then pivot back to wide format
-  palt_v7 <- tidyr::pivot_wider(data = palt_v6,
-                             names_from = "color_num",
-                             values_from = "color_hex")
+  palt_v7 <- tidyr::pivot_wider(data = palt_v6, names_from = "color_num", values_from = "color_hex")
   
   # Make it a dataframe
   palt <- as.data.frame(palt_v7)
   
-  # Now check for some obvious errors
-  ## No palette meets specified conditions
+  # Return informative messages based on outcome
   if(nrow(palt) == 0){ print("No palette met the user-supplied conditions. Run function without specifying any arguments to see available palette options")
     return(palt) } else {
-  
-  ## More than 1 palette returned
+      
   if(nrow(palt) > 1){
     print("Multiple options returned as a dataframe. Consider specifying subset and re-running function.")
     return(palt) } else {
   
-  ## Exactly one palette identified
   if(nrow(palt == 1)){
     print("Exactly one palette identified. Output cropped to only HEX codes for ease of plotting")
     palt_simp <- as.vector(dplyr::select(.data = palt, dplyr::starts_with('color')))
     colnames(palt_simp) <- NULL
     return(palt_simp)
   } } } }
-
-# Test the Function ----------------------------------------------------------
-
-# To see all the options, simply run the function without specifying arguments
-lter_palette_finder()
-
-# What if our query returns NO options?
-test_palt <- lter_palette_finder(length = 1)
-test_palt
-
-# What if our query returns MULTIPLE options?
-test_palt <- lter_palette_finder(length = 5, site = "HBR")
-test_palt
-
-# What if our query returns JUST ONE option? (this is desirable)
-test_palt <- lter_palette_finder(site = "AND")
-test_palt
-
-# Test plot
-library(ggplot2)
-( df <- data.frame(x = c("a", "b", "c", "d", "e"), y = 1:5) )
-ggplot(data = df, aes(x = x, y = y, fill = x)) +
-  geom_bar(stat = 'identity') +
-  scale_fill_manual(values = test_palt) +
-  theme_classic() + theme(legend.position = "none")
-
-
-
-# End ------------------------------------------------------------------------
