234ea3c2f75ee2a04e654a032c2cd5eed6852b14
njlyon0 <nicholasjlyon@gmail.com>
2022-05-13 14:21:29 -0500

    Increased flexibility to accept a vector of HEX codes less than or equal to 25 (previously required exactly 25)

--- R/palette_demo.R
+++ R/palette_demo.R
@@ -2,7 +2,7 @@
 #' 
 #' @description Accepts the hexadecimal code vector returned by `palette_extract()` or `palette_sort()` and creates a base plot of all returned colors labeled with their HEX codes. This will facilitate (hopefully) your selection of which of the 25 colors you would like to use in a given context.
 #' 
-#' @param palette Vector returned by `palette_extract()` or `palette_sort()`
+#' @param palette Vector of hexadecimal codes like that returned by `palette_extract()` or `palette_sort()`
 #' @param export Logical of whether or not to export the demo plot
 #' @param export_name Character string of what to name exported plot
 #' @param export_path File path to save exported plot (defaults to working directory)
@@ -22,42 +22,44 @@ palette_demo <- function(palette, export = FALSE,
                          export_name = "my_palette",
                          export_path = getwd()){
   
-  # Strip out the hex codes to use as a vector later
-  hexes <- palette
-  
   # If the user doesn't want to export the plot...
   if(export == FALSE) {
     # Just plot the colors
     base::plot(x = 1:base::length(palette),
-               y = base::rep(x = c(1, 3, 5, 2, 4), times = 5),
-               col = hexes, pch = 19, cex = 6,
+               y = base::rep(x = c(1, 3, 5, 2, 4),
+                             times = 5)[1:base::length(palette)],
+               col = palette, pch = 19, cex = 6,
                xlab = '', ylab = '',
                xaxt = "n", yaxt = "n",
                ylim = c(0.5, 5.5),
                xlim = c(-1, base::length(palette) + 1))
     # And add the hex codes on top of the colors
     graphics::text(x = 1:base::length(palette),
-         y = rep(x = c(1, 3, 5, 2, 4), times = 5),
+         y = rep(x = c(1, 3, 5, 2, 4),
+                 times = 5)[1:base::length(palette)],
          labels = palette, cex = 0.6)
   }
   
   # If the user does want to export, export it!
   if(export == TRUE){
     grDevices::jpeg(
-      filename = base::file.path(export_path, base::paste0(export_name, ".jpg")),
+      filename = base::file.path(export_path,
+                                 base::paste0(export_name, ".jpg")),
          quality = 600, res = 1080,
          width = 4, height = 4, units = 'in')
-    # Just plot the colors
+    # Plot the colors
     base::plot(x = 1:base::length(palette),
-               y = base::rep(x = c(1, 3, 5, 2, 4), times = 5),
-               col = hexes, pch = 19, cex = 6,
+               y = base::rep(x = c(1, 3, 5, 2, 4),
+                             times = 5)[1:base::length(palette)],
+               col = palette, pch = 19, cex = 6,
                xlab = '', ylab = '',
                xaxt = "n", yaxt = "n",
                ylim = c(0.5, 5.5),
                xlim = c(-1, base::length(palette) + 1))
     # And add the hex codes on top of the colors
     graphics::text(x = 1:base::length(palette),
-                   y = rep(x = c(1, 3, 5, 2, 4), times = 5),
+                   y = rep(x = c(1, 3, 5, 2, 4),
+                           times = 5)[1:base::length(palette)],
                    labels = palette, cex = 0.6)
     # And dev.off out
     grDevices::dev.off()
