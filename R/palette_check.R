a6ec6f4793d2d4e87778d2a402261130356adb36
njlyon0 <nicholasjlyon@gmail.com>
2022-07-29 11:49:29 -0500

    Streamlined `palette_check` to reject hexcodes that are incorrect character lengths and check the contents of hexcodes that are the correct number of characters

--- R/palette_check.R
+++ R/palette_check.R
@@ -1,10 +1,9 @@
 #' @title Check Hexadecimal Code Formatting
 #'
-#' @description Accepts the hexadecimal code vector and test if it is formatted correctly
+#' @description Accepts the hexadecimal code vector and tests if it is formatted correctly
 #' 
-#' @param palette Vector of hexadecimal codes returned by `palette_extract()`, `..._sort()`, or `..._subsample()`
+#' @param palette (character) Vector of hexadecimal codes returned by `palette_extract()`, `..._sort()`, or `..._subsample()`
 #' 
-#'
 #' @return An error message or nothing
 #' @export
 #'
@@ -16,14 +15,14 @@
 palette_check <- function(palette){
 
   # Check if palette is a character vector
-  if(!is.character(palette)) {
+  if(!is.character(palette))
     stop("A character vector was expected")
-  }
+  
+  # Check if any palettes have too many/few characters
+  if(sum(nchar(palette) != 7) > 0)
+    stop("Hexadecimal codes must be 7 digits (including the '#')")
   
   # Check if you have correct hexadecimal codes
-  if(sum(!grepl('^#[A-Fa-f0-9]{6}$', palette)) > 0){
-    if(sum(!grepl('^#[A-Fa-f0-9]{8}$', palette)) > 0){
-      stop("Some hexadecimal codes are not correctly formatted")
-    }
-  }
+  if(sum(!grepl('^#[A-Fa-f0-9]{6}$', palette)) > 0)
+    stop("Some hexadecimal codes are not correctly formatted")
 }
