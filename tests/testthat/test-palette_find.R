8ccfab242b8be40c228c0009a921d52337755f5d
njlyon0 <nicholasjlyon@gmail.com>
2022-07-29 10:57:07 -0500

    Added `testthat` architecture for when I build custom tests of the package

---
+++ tests/testthat/test-palette_find.R
@@ -0,0 +1,3 @@
+# Don't yet have any tests but want to preserve info on how to create tests going forward if desired
+# See Hadley Wickham's instructions for testing basics here:
+# https://r-pkgs.org/testing-basics.html
