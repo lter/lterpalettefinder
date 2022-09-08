877e4e553ee86757c65e368b1c7ea416c4ea6ed8
njlyon0 <nicholasjlyon@gmail.com>
2022-09-08 14:50:04 -0400

    Set up the script that will update the `palette_options` as new photos are found / added

--- update-palette-options/embed-new-palettes.R
+++ update-palette-options/embed-new-palettes.R
@@ -52,13 +52,6 @@ all_photos <- photo_names %>%
                   remove = FALSE, sep = "\\.")
 head(all_photos)
 
-# To save on processing time, drop the photos we already have palettes for
-new_photos <- all_photos
-# new_photos <- all_photos %>%
-#   dplyr::filter(photo_title %in% setdiff(photo_title,
-#                                         palette_options$palette_full_name))
-# new_photos
-
 # Save import/export paths as objects
 import_path <- file.path("update-palette-options", "official photos")
 export_path <- file.path("update-palette-options", "official palettes")
@@ -67,10 +60,10 @@ export_path <- file.path("update-palette-options", "official palettes")
 palette_list <- list()
 
 # For each new photo:
-for(image in new_photos$file_names){
+for(image in all_photos$file_names){
   
   # Identify the photo title (i.e., no file extension)
-  title <- new_photos$photo_title[new_photos$file_names == image]
+  title <- all_photos$photo_title[all_photos$file_names == image]
   
   # Extract color
   colors <- palette_extract(image = file.path(import_path, image),
@@ -81,7 +74,7 @@ for(image in new_photos$file_names){
                export_name = file.path(export_path, title))
   
   # Add to list
-  palette_list[[title]] <- data.frame("source" = title,
+  palette_list[[title]] <- data.frame("palette_full_name" = title,
                                       "hexes" = colors)
   
   # Print message
@@ -89,168 +82,153 @@ for(image in new_photos$file_names){
   
 }
 
-# Check the resulting list briefly
-str(palette_list)
+# Bind the list into a dataframe to make it easier to wrangle
+palette_df_v0 <- palette_list %>%
+  purrr::map_dfr(.f = dplyr::bind_rows)
+
+# Add the LTER logo palette manually
+lter_palette <- data.frame("palette_full_name" = "LTER-logo",
+                           "hexes" = c("#5A5C37", "#209ACA", "#97AE3F",
+                                       "#FFDF1D", "#4AB2CB", "#DA6641",
+                                       "#B89E92", "#374228"))
+
+# Add it to the extracted palettes
+palette_df <- lter_palette %>%
+  dplyr::bind_rows(palette_df_v0)
+
+# Glimpse that
+dplyr::glimpse(palette_df)
 
 ## --------------------------------------------- ##
           # Wrangle Extracted Palettes ----
 ## --------------------------------------------- ##
-
-
-
-
-palette_list
-
-
-# LTER logo palette
-c("#5A5C37", "#209ACA", "#97AE3F", "#FFDF1D", "#4AB2CB", "#DA6641", "#B89E92", "#374228")
-
-
-# Vital stats of pics
-
-
-
-
-# AND-reu | Lina DiGregorio
-# AND-salamander | Chris Cousins
-# AND-training | Joelle Worthley
-
-# ARC-aurora | Jansen Nipko
-# ARC-autumn | Lindsay VanFossen
-# ARC-bucket | Jansen Nipko
-# ARC-hike | Abigail Rec
-# ARC-sunrise | Lindsay VanFossen
-# ARC-sunset | Lindsay VanFossen
-
-# BLE-icedrill | Ken Dunton
-# BLE-sunset | Kaylie Plumb
-
-# CAP-cactus | Quincy Stewart
-# CAP-lovebird | Maddy Gibson
-
-# HBR-canopy | Ashley Lang
-# HBR-lotus | Ashley Lang
-# HBR-mushroom floor | Ashley Lang
-# HBR-mushroom tree | Jackie Matthes
-
-
-# JRN-calf | Dylan Stover
-# JRN-monsoon | Ryan Schroeder
-# JRN-oryx | Ryan Schroeder
-# JRN-rainbow | Ryan Schroeder
-# JRN-sunset | Ryan Schroeder
-
-# KBS-birds | Michaela Rose
-# KBS-fire | Yahn-Jauh Su
-# KBS-morning | Kara Dobson
-# KBS-storm | Yahn-Jauh Su
-
-# KNZ-bison | Jill Haukos
-# KNZ-burn | Jill Haukos
-# KNZ-nightfire | Jill Haukos
-# KNZ-regal | Jill Haukos
-
-# MCR-lagoon | Kathryn Scafidi
-
-# NGA-boat | Emily Stidham
-
-# NWT-mountain dawn | Chiara Forester
-# NWT-mountain field | Chiara Forester
-# NWT-mountain flowers | Chiara Forester
-# NWT-snowy mountain | Chiara Forester
-# NWT-snowy trees | Chiara Forester
-# NWT-wildflowers | Chiara Forester
-
-# PAL-boat | Andrew Corso
-# PAL-net | Andrew Corso
-# PAL-penguins | Andrew Corso
-# PAL-squid | Andrew Corso
-
-# SBC-kelp tag | Kyle Emery
-
-# VCR-boat | Amelie Berger
-# VCR-marsh | Sophia Hoffman
-
-
-# Extract Palette from Photos -------------------------
-
-# Identify file name (w/o suffix)
-photo_name <- "fire"
-
-# Extract color
-colors <- lterpalettefinder::palette_extract(image = file.path("Photos", paste0(photo_name, ".png")))
-
-# Take a look at that object
-head(colors)
-
-# Plot the extracted palette
-lterpalettefinder::palette_demo(palette = colors, export = T, export_name = file.path("Palettes", paste0(photo_name, "-palette")))
-
-# Make a dataframe
-color_df <- data.frame("source" = photo_name, "hex_code" = colors)
-head(color_df)
-
-# Export the .csv if need be
-write.csv(x = color_df, row.names = F, file = file.path("Palettes", paste0(photo_name, "-palette-df.csv")))
-
-# Check Official Palettes -----------------------------------------
-# After you've picked 10 colors from a new picture and added them to the csv, read the csv in and take a look at how the colors look when they're in a graph together in the order you entered them.
-# If needed, revise your entry for that row and re-load in the .csv
-
-# Read in data
-# palette_options <- lterpalettefinder::palette_find()
-palette_options <- read.csv(file = file.path('Data', 'palette_options.csv'))
-
-# Choose which row (i.e., which palette) to graph
-row_num <- 14
-
-# Prepare a palette
-specific_palette <- palette_options[row_num, 7:ncol(palette_options)] %>%
-  # Pivot to long format
-  tidyr::pivot_longer(cols = dplyr::everything(),
-                      names_to = "color_num",
-                      values_to = "hex") %>%
-  # Drop colors that aren't in the given palette
-  dplyr::filter(nchar(hex) != 0) %>%
+# Now that we have palettes and titles, we have additional wrangling to do
+palette_actual <- palette_df %>%
+  # Split the title apart into the site abbreviation and the photo name
+  ## But we'll keep the combined version
+  tidyr::separate(col = palette_full_name,
+                  into = c("lter_site", "palette_name"),
+                  sep = "-", remove = FALSE) %>%
+  # Manually pull photographer name based on the photo title
+  dplyr::mutate(photographer = dplyr::case_when(
+    palette_full_name == "LTER-logo" ~ "LTER Network Office",
+    # Andrews Forest (AND)
+    palette_full_name == "AND-reu" ~ "Lina DiGregorio",
+    palette_full_name == "AND-salamander" ~ "Chris Cousins",
+    palette_full_name == "AND-training" ~ "Joelle Worthley",
+    # Arctic (ARC)
+    palette_full_name %in% c("ARC-aurora", "ARC-bucket") ~ "Jansen Nipko",
+    palette_full_name %in% c("ARC-autumn", "ARC-sunrise",
+                             "ARC-sunset") ~ "Lindsay VanFossen",
+    palette_full_name == "ARC-hike" ~ "Abigail Rec",
+    # Beaufort Lagoon Ecosystems (BLE)
+    palette_full_name == "BLE-ice drill" ~ "Ken Dunton",
+    palette_full_name == "BLE-sunset" ~ "Kaylie Plumb",
+    # Central Arizona–Phoenix (CAP)
+    palette_full_name == "CAP-cactus" ~ "Quincy Stewart",
+    palette_full_name == "CAP-lovebird" ~ "Maddy Gibson",
+    # Hubbard Brook
+    palette_full_name %in% c("HBR-canopy", "HBR-lotus",
+                             "HBR-mushroom floor") ~ "Ashley Lang",
+    palette_full_name == "HBR-mushroom tree" ~ "Jackie Matthes",
+    # Jornada Basin (JRN)
+    palette_full_name == "JRN-calf" ~ "Dylan Stover",
+    palette_full_name %in% c("JRN-monsoon", "JRN-oryx", "JRN-rainbow",
+                             "JRN-sunset") ~ "Ryan Schroeder",
+    # Kellogg Biological Station
+    palette_full_name == "KBS-birds" ~ "Michaela Rose",
+    palette_full_name == "KBS-morning" ~ "Kara Dobson",
+    palette_full_name %in% c("KBS-fire", "KBS-storm") ~ "Yahn-Jauh Su",
+    palette_full_name == "KBS-swallowtail" ~ "Corinn Rutkoski",
+    # Konza Prairie (KNZ)
+    palette_full_name %in% c("KNZ-bison", "KNZ-burn", "KNZ-night fire",
+                             "KNZ-regal") ~ "Jill Haukos",
+    # Moorea Coral Reef (MCR)
+    palette_full_name == "MCR-lagoon" ~ "Kathryn Scafidi",
+    # Northern Gulf of Alaska (NGA)
+    palette_full_name == "NGA-boat" ~ "Emily Stidham",
+    # Niwot Ridge (NWT)
+    palette_full_name %in% c("NWT-mountain dawn", "NWT-mountain field", "NWT-mountain flowers", "NWT-snowy mountain", "NWT-snowy trees", "NWT-wildflowers") ~ "Chiara Forester",
+    # Palmer Station (PAL)
+    palette_full_name %in% c("PAL-boat", "PAL-net", "PAL-penguins",
+                             "PAL-squid") ~ "Andrew Corso",
+    # Santa Barbara Coastal (SBC)
+    palette_full_name == "SBC-kelp tag" ~ "Kyle Emery",
+    # Virginia Coast (VCR)
+    palette_full_name == "VCR-boat" ~ "Amelie Berger",
+    palette_full_name == "VCR-marsh" ~ "Sophia Hoffman",
+    # If you don't specify a photographer, "MISSING" will be entered
+    TRUE ~ "MISSING") ) %>%
+  # Also assign a type of palette to each palette
+  dplyr::mutate(palette_type = dplyr::case_when(
+    # Sequential - colors roughly follow a *single* gradient
+    palette_full_name %in% c("AND-training", "ARC-aurora", "ARC-sunset", "HBR-canopy", "JRN-oryx", "KBS-birds", "NWT-snowy trees", "PAL-squid"
+                             ) ~ "sequential",
+    # Diverging - colors follow *two gradients*
+    palette_full_name %in% c("ARC-hike", "ARC-sunrise", "BLE-sunset", "CAP-cactus", "JRN-monsoon", "JRN-rainbow", "JRN-sunset", "KBS-morning", "KBS-storm", "KNZ-bison", "NWT-mountain dawn", "NWT-mountain field", "NWT-mountain flowers", "NWT-snowy mountain", "PAL-net", "VCR-marsh"
+                             ) ~ "diverging",
+    # Tricolor - colors mostly fall into three groups
+    palette_full_name %in% c("AND-salamander", "ARC-autumn", "HBR-mushroom floor", "HBR-mushroom tree", "KBS-fire", "KNZ-night fire", "MCR-lagoon", "NWT-wildflowers", "PAL-penguins"
+                             ) ~ "tricolor",
+    # Qualitative - colors do not follow a gradient and/or seem random
+    palette_full_name %in% c("LTER-logo", "AND-reu", "ARC-bucket", "BLE-ice drill", "CAP-lovebird", "HBR-lotus", "JRN-calf", "KBS-swallowtail", "KNZ-burn", "KNZ-regal", "NGA-boat", "PAL-boat", "SBC-kelp tag", "VCR-boat"
+                             ) ~ "qualitative",
+    # If the photo name isn't included in one of these three, "MISSING" will be entered
+    TRUE ~ "MISSING") ) %>%
+  # Create a color number column
+  dplyr::group_by(palette_full_name) %>%
+  dplyr::mutate(color_number = paste0("color_", seq_along(hexes))) %>%
+  dplyr::ungroup() %>%
+  # Reorder columns
+  dplyr::select(photographer, palette_full_name, lter_site, palette_name,
+                palette_type, color_number, hexes) %>%
+  # Pivot to wide format
+  tidyr::pivot_wider(
+    id_cols = photographer:palette_type,
+    names_from = color_number,
+    values_from = hexes) %>%
   # Make it a dataframe
   as.data.frame()
 
-# Do an exploratory ggplot!
-lterpalettefinder::palette_ggdemo(palette = specific_palette$hex)
-
-# End ----
+# Take another look
+dplyr::glimpse(palette_actual)
 
 
+# JRN-pronghorn | Brandon Bestelmeyer
 
 
-## -------------------------------------------------- ##
-# Save Palette .csv as .rda ----
-## -------------------------------------------------- ##
-# Written by Nick Lyon
+# Check that all photos have a photographer and palette type identified
+## Photographer
+palette_actual$palette_full_name[palette_actual$photographer == "MISSING"]
+## Go up and add photographers to any photos that pop up here
 
-# Save CSV as RDA --------------------------------------
+## Palette type
+palette_actual$palette_full_name[palette_actual$palette_type == "MISSING"]
+## Go up and add palette types to any photos that pop up here
 
-# NOTE:
-## This script assumes that you just edit the .csv via your preferred method and then run this script. Be sure to close the .csv when you're done editing but **before** running this script.
+## --------------------------------------------- ##
+     # Export Updated Official Palettes ----
+## --------------------------------------------- ##
 
-# Clear environment
-rm(list = ls())
+# Take one last look
+head(palette_actual)
 
-# Read in data
-palette_options <- read.csv(file = file.path('Data', 'palette_options.csv'))
+# Re-name it
+palette_options <- palette_actual
 
 # Save it as a .rda
-base::save(palette_options, file = file.path('Data', 'palette_options.rda'))
-
-# Check that worked ------------------------------------
+base::save(palette_options, file = file.path("data", "palette_options.rda"))
 
 # Clear environment
 rm(list = ls())
 
-# Load the file
-load(file.path('Data', 'palette_options.rda'))
+# Load the `palette_options` file to make sure that worked
+load(file.path("data", "palette_options.rda"))
 
 # Examine structure
-str(palette_options)
+dplyr::glimpse(palette_options)
 
-# End --------------------------------------------------
+# Clear environment
+rm(list = ls())
+
+# End ----
