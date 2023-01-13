d33316f28f1030e5a38771da565e9bea58a1ed2d
njlyon0 <nicholasjlyon@gmail.com>
2023-01-13 10:00:45 -0500

    Tweaked to explore issue #7

--- troubleshooting/workbench.R
+++ troubleshooting/workbench.R
@@ -7,9 +7,9 @@
 ## Allows using `devtools::load_all()` to +/- instantly load latest changes
 
 ## ------------------------------------- ##
-  # Issue #6: Images with <25 Colors ----
+  # Issue #7: Hex Mis-Transformation ----
 ## ------------------------------------- ##
-# https://github.com/lter/lterpalettefinder/issues/6
+# https://github.com/lter/lterpalettefinder/issues/7
 
 # Clear environment
 rm(list = ls())
@@ -17,17 +17,37 @@ rm(list = ls())
 # Load lterpalettefinder **development version**
 devtools::load_all()
 
-# Use palette_extract on the test image
-test <- palette_extract(image = file.path("troubleshooting", "simple_image.png"), 
-                        sort = T, progress_bar = T)
-## Error in sample.int(m, k) : 
-## cannot take a sample larger than the population when 'replace = FALSE'
-
 # Assign image object to use guts of function more directly
-image <- file.path("troubleshooting", "simple_image.png")
+image_path <- file.path("troubleshooting", "simple_image.png")
 
-# Read it in
-pic <- png::readPNG(source = image, native = FALSE)
+# Use palette_extract on the test image
+(test <- lterpalettefinder::palette_extract(image = image_path, progress_bar = T))
+
+# Check it
+palette_ggdemo(palette = test)
+
+# Create a set of integers <10 (i.e., those that should be "0x" as hexcodes)
+(test_ints <- base::as.integer(x = 1:9))
+
+# Force them to be hexadecimals
+(test_hex <- base::as.hexmode(test_ints))
+
+# Make them characters
+(test_char <- base::as.character(test_hex))
+
+# What if it was a dataframe?
+(test_df <- data.frame("ints" = test_ints))
+
+# Now make it hexadecimal
+(as.hexmode(test_df$ints))
+
+# Now assemble them into a faux hexcode
+paste0("#", test_char[1], test_char[2], test_char[3])
+## Would be inappropriate because doesn't make them two digits
+
+# Now try with guts of `palette_extract`
+pic <- png::readPNG(source = image_path, native = FALSE)
+# pic <- jpeg::readJPEG(source = image, native = FALSE)
 
 # Extract RGB channels
 rawR <- base::as.integer(pic[,,1] * 255)
@@ -37,17 +57,15 @@ rawB <- base::as.integer(pic[,,3] * 255)
 # Put them into a dataframe
 rgb_v1 <- base::data.frame("red" = rawR, "green" = rawG, "blue" = rawB)
 
-### Check that out
-head(rgb_v1)
-
-# We want to subset out very dark colors
+# Subset out very dark colors (i.e., those with RGB values all below a threshold)
 rgb_v2 <- rgb_v1 %>%
   dplyr::filter(!(red < 65 & green < 65 & blue < 65))
 
-# Return unique values
-rgb_v3 <- unique(rgb_v2)
+# Return only unique values
+rgb_v3 <- base::unique(rgb_v2)
 
 # If >25 colors, do k-means clustering on these RGB values
+## More than 25 colors
 if(nrow(rgb_v3) > 25){
   rgb_v4 <- base::as.data.frame(
     base::suppressWarnings(
@@ -55,9 +73,6 @@ if(nrow(rgb_v3) > 25){
                     iter.max = 100, nstart = 1)$centers)) }
 ## Fewer than 25 colors
 if(nrow(rgb_v3) <= 25){ rgb_v4 <- rgb_v3 }
-
-### Check that result out
-rgb_v4
 
 # Turn them into integers (instead of continuous numbers)
 rgb_v5 <- rgb_v4 %>%
@@ -70,6 +85,11 @@ hexR <- base::as.hexmode(rgb_v5$red)
 hexG <- base::as.hexmode(rgb_v5$green)
 hexB <- base::as.hexmode(rgb_v5$blue)
 
+# If any of those are one digit (i.e., 0:9), add a leading zero
+if(base::nchar(hexR) == 1){ hexR <- base::paste0("0", base::as.character(hexR)) }
+if(base::nchar(hexG) == 1){ hexG <- base::paste0("0", base::as.character(hexG)) }
+if(base::nchar(hexB) == 1){ hexB <- base::paste0("0", base::as.character(hexB)) }
+
 # Bind hexadecimals into HEX codes
 hex_vec <- base::paste0('#',
                         base::as.character(hexR),
@@ -81,17 +101,6 @@ hexes <- base::data.frame(hex_code = base::unique(hex_vec))
 
 # Make it a vector
 hex_vec <- base::as.character(hexes$hex_code)
-
-# Sort in a similar way to human eye
-hex_sort <- lterpalettefinder::palette_sort(palette = hex_vec)
-
-# Do exploratory plotting with this
-palette_demo(palette = hex_sort)
-palette_ggdemo(palette = hex_sort)
-
-# Export one of these plots (if desired)
-ggplot2::ggsave(file.path("troubleshooting", "simple_palette.jpg"),
-                height = 2, width = 3, units = "in")
 
 # Clear environment again
 rm(list = ls())
